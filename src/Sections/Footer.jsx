a1edc2049ae15f66aaffb56a7bf74875823ec1e0
Rahul Nagarajan <rahulnagarajan@Rahuls-MacBook-Air.local>
2024-09-20 15:22:58 +0530

    Added footer

---
+++ src/Sections/Footer.jsx
@@ -0,0 +1,51 @@
+import { PiArrowFatLineRightThin } from "react-icons/pi";
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+
+
+const Footer = () => {
+    return <footer className="bg-secondaryColor py-[5rem]">
+        <div className="lg:w-[70%] w-[90%] mx-auto gap-[2rem] flex lg:flex-row flex-col-reverse items-center lg:justify-between lg:items-start">
+            <div className="flex flex-col gap-4">
+                <p className="text-desc">We Leverage Cutting Edge Technologies</p>
+                <div className="flex">
+                    <input type="text" placeholder="Email Address" className="px-[0.9rem] w-[80%] py-[0.8rem] text-[0.8rem]"></input>
+                    <button className="bg-primaryColor w-[20%] border-[7px] border-white flex justify-center items-center">{<PiArrowFatLineRightThin style={{ color: "white" }} />}</button>
+                </div>
+                <div className="flex flex-row lg:justify-start justify-center gap-3">
+                    <a href="#" className=" text-gray-400 text-[1.8rem] rounded-[50%]"><FaFacebook /></a>
+                    <a href="#" className=" text-gray-400 text-[1.8rem] rounded-[50%]"><FaTwitter /></a>
+                    <a href="#" className=" text-gray-400 text-[1.8rem] rounded-[50%]"><FaInstagramSquare /></a>
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-1  lg:items-start items-center justify-start">
+                <p className="text-sec-head text-white my-2">Our Links</p>
+                <a href="#" className="text-desc">Home</a>
+                <a href="#" className="text-desc">About Us</a>
+                <a href="#" className="text-desc">Services</a>
+                <a href="#" className="text-desc">Portfolio</a>
+                <a href="#" className="text-desc">Contact Us</a>
+            </div>
+
+            <div className="flex flex-col gap-3 lg:w-[33%] lg:items-start items-center">
+                <p className="text-sec-head text-white my-2">Contact Us</p>
+                <div className="flex flex-row justify-center items-center gap-4">
+                    <div className="bg-primaryColor min-w-[2rem] min-h-[2rem] rounded-[50%]"></div>
+                    <p className="text-desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, quibusdam!</p>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-4">
+                    <div className="bg-primaryColor min-w-[2rem] min-h-[2rem] rounded-[50%]"></div>
+                    <p className="text-desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, quibusdam!</p>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-4">
+                    <div className="bg-primaryColor min-w-[2rem] min-h-[2rem] rounded-[50%]"></div>
+                    <p className="text-desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, quibusdam!</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+}
+
+export default Footer
